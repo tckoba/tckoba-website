6348a89722616d51ae00cd8a291c336c42f02edf
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-02-01 08:48:41 +0530

    [OW-11]: added basic routes

---
+++ src/views/Events.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="events">
+    <h1>This is an events page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Events",
+};
+</script>
---
+++ src/views/Stories.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="stories">
+    <h1>This is an stories page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stories",
+};
+</script>
