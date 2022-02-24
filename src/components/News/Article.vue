912f554b04760c71c16ca24892ea2e049505373f
akilax0 <killerilaksha@gmail.com>
2022-02-24 21:34:13 +0530

    News init

---
+++ src/components/News/Article.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="article">
+        Article
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'article',
+    props: ['article']
+}
+</script>
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="news">
-    <h1>This is an news page</h1>
+    <Article/>
   </div>
 </template>
 
 <script>
+import Article from '../components/News/Article.vue';
+
 export default {
   name: "News",
+  components : {
+    Article
+  },
+  data(){
+    return {
+      products: [
+        {
+          title: '125 Year Celebrations',
+          color: 'Red',
+          bgtext: 'AIR',
+          src: require('../assets/News/125.jpg')
+        },
+      ]
+    }
+  }
 };
 </script>
