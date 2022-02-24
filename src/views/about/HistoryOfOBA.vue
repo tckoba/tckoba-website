c1d66d1772ee8425ea12e00a08da89b696e351dc
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-02-24 21:27:35 +0530

    [OW-12]: added basic structure and content to history page

--- src/views/about/HistoryOfOBA.vue
+++ src/views/about/HistoryOfOBA.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an history page</h1>
+  <div>
+    <Jumbotron :heading="heading" :image="jumbrotronImage" />
+    <v-container class="body">
+      <v-row>
+        <v-col class="body__content">
+          <p v-for="(para, i) in body" :key="i">{{ para }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import Jumbotron from "../../components/Jumbotron.vue";
+import jumbrotronImage from "../../assets/historyOfOBA.png";
+import { heading, body } from "../../data/historyOfOBA.json";
 export default {
   name: "HistoryOfOBA",
+  components: { Jumbotron },
+  data: () => ({
+    jumbrotronImage,
+    heading,
+    body,
+  }),
 };
 </script>
+
+<style scoped>
+.body__content {
+  margin: 50px auto;
+  background-color: #f9f9f9;
+  padding: 50px;
+  text-align: justify;
+}
+</style>
