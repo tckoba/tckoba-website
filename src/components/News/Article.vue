57ceefa2e16e5a8e7d5d457f7f9f9a219d676741
akilax0 <killerilaksha@gmail.com>
2022-02-24 23:22:39 +0530

    card components for articles

--- src/components/News/Article.vue
+++ src/components/News/Article.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="article">
-        Article
+        <div class="article-inner">
+            <div class="article-image-wrap">
+                <img :src="article.src" class="image"/>
+            </div>
+            <div class="article-detail">
+                <h2>{{article.title}}</h2>
+                <p>{{article.description}}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,3 +18,27 @@ export default{
     props: ['article']
 }
 </script>
+
+<style>
+    .article {
+        flex: 1 1 33.333%;
+        width: 100%;
+        padding:25px;
+    }
+
+    .article-inner{
+        position: relative;
+        padding: 25px;
+        box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+        perspective: 1000px;
+    }
+
+    .article-image-wrap .image{
+        width: 100%;
+        filter: drop-shadow(0px 0px 12px rgba(0,0,0,0.25))
+    }
+
+    
+
+</style>
+
