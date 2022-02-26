ce85cd07f6896e4325a2766840e704cbe3a8e424
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-02-26 22:38:01 +0530

    [OW-19]: updated trinity stories route and added json data

---
+++ src/views/TrinityStories.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Jumbotron :heading="heading" :image="jumbrotronImage" />
+    <PageIntroduction :content="introduction" />
+  </div>
+</template>
+
+<script>
+import Jumbotron from "../components/Jumbotron.vue";
+import PageIntroduction from "../components/PageIntroduction.vue";
+import jumbrotronImage from "../assets/trinity-stories.png";
+import { heading, introduction, stories } from "../data/trinity-stories.json";
+export default {
+  name: "HistoryOfOBA",
+  components: { Jumbotron, PageIntroduction },
+  data: () => ({
+    jumbrotronImage,
+    heading,
+    introduction,
+    stories,
+  }),
+};
+</script>
