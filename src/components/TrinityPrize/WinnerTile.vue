47fa6b998fbd778fd4671bcfbdddc1f636ec47dd
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-02-27 23:06:25 +0530

    [OW-20]: added basic structure for trinitu prize page

---
+++ src/components/TrinityPrize/WinnerTile.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-avatar size="150">
+      <v-img :src="winner.image"> </v-img>
+    </v-avatar>
+    <v-card-title>{{ winner.name }}</v-card-title>
+    <v-card-subtitle>{{ winner.category }}</v-card-subtitle>
+    <v-card-text> {{ winner.description }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "WinnerTile",
+  props: ["winner"],
+};
+</script>
+
+<style>
+</style>
