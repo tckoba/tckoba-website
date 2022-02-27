2ba557d3b1681afcc475587651aef07a73f6691c
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-02-28 00:15:32 +0530

    [OW-25]: created events jumbotron image in assets

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -3,23 +3,22 @@
     <Jumbotron :heading="heading" :image="jumbrotronImage" />
     <v-container>
       <v-row>
-        <Event v-for="(event,i) in events" :key="i" :event="event" />
+        <Event v-for="(event, i) in events" :key="i" :event="event" />
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-
 import Jumbotron from "../components/Jumbotron.vue";
-import jumbrotronImage from "../assets/historyOfOBA.png";
-import Event from '../components/Events/Event.vue';
-import { heading ,events} from "../data/events.json";
+import jumbrotronImage from "../assets/events/events.png";
+import Event from "../components/Events/Event.vue";
+import { heading, events } from "../data/events.json";
 
 export default {
   name: "Events",
-  components: {Event,Jumbotron},
-  data: ()=>({
+  components: { Event, Jumbotron },
+  data: () => ({
     jumbrotronImage,
     heading,
     events,
