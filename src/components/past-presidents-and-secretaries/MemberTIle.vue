6764e413a26954652ff23ca9487e405303593133
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-03-19 10:56:57 +0530

    [OW-16]:added president and secretary structure

---
+++ src/components/past-presidents-and-secretaries/MemberTIle.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card flat>
+    <v-row>
+      <v-col cols="auto">
+        <v-avatar color="primary" size="75"></v-avatar>
+      </v-col>
+      <v-col>
+        <v-card-title>{{ name }}</v-card-title>
+        <v-card-subtitle>{{ designation }}</v-card-subtitle>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "MemberTile",
+  props: ["name", "designation"],
+};
+</script>
+
+<style>
+</style>
---
+++ src/data/pastPresidentsAndSecretaries.js
@@ -0,0 +1,123 @@
+export const heading = "Past Presidents And Secretaries",
+  decades = [
+    {
+      decade: "2021-2030",
+      members: [
+        {
+          year: "2021",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+      ],
+    },
+    {
+      decade: "2011-2020",
+      members: [
+        {
+          year: "2020",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2019",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2018",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2017",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2016",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2015",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2014",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2013",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2012",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2011",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+      ],
+    },
+    {
+      decade: "2001-2010",
+      members: [
+        {
+          year: "2010",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2009",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2008",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2007",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2006",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2005",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2004",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2003",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2002",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+        {
+          year: "2001",
+          president: "Mr.President",
+          secretary: "Mr.Secretary",
+        },
+      ],
+    },
+  ];
