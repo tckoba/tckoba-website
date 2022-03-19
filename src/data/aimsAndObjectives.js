c4e9708f4cec1ee4a6e662c9bde90e6ebfd7eab1
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-03-19 12:38:10 +0530

    [OW-14]: added basic aims and objectives

---
+++ src/data/aimsAndObjectives.js
@@ -0,0 +1,11 @@
+export const heading = "Aims & Objectives",
+  content = [
+    "To increase fellowship amongst Old Boys  both with one another and with Trinity College Kandy",
+    "To approve and recognize the formation of Branch Associations in various parts of Sri Lanka and abroad and to co-ordinate the activities at all Old Boys’ Branch Associations and of those that may be formed hereafter",
+    "To make recommendations to the Board of Governors in areas relating to the management and administration of Trinity College",
+    "To give opportunities to the Old Boys to contribute to the life of Trinity College by their support and assistance in every manner",
+    "To maintain a register of members of the Association",
+    "To organize functions to felicitate Old Boys of Trinity College or any other persons associated with the College, who have rendered distinguished service to Trinity College or to the Association and to organize reunions and other functions",
+    "To facilitate career guidance programmes which would assist senior students of Trinity College in obtaining employment",
+    "To create a Trust Fund",
+  ];
