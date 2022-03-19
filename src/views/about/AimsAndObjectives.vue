e621d38b834b99157b3dbcbe3e635376db85096b
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-03-19 12:38:10 +0530

    [OW-14]: added basic aims and objectives

--- src/views/about/AimsAndObjectives.vue
+++ src/views/about/AimsAndObjectives.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an AimsAndObjectives page</h1>
+  <div>
+    <Jumbotron :heading="heading" :image="jumbrotronImage" />
+    <v-container>
+      <v-row>
+        <v-col class="body__content">
+          <p>The aims and objectives of the “ASSOCIATION” shall be,</p>
+          <ul>
+            <li v-for="(item, i) in content" :key="i">{{ item }}</li>
+          </ul>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import Jumbotron from "../../components/Jumbotron.vue";
+import jumbrotronImage from "../../assets/history-of-oba/historyOfOBA.png";
+import { heading, content } from "../../data/aimsAndObjectives.js";
 export default {
-  name: "AimsAndObjectives",
+  name: "HistoryOfOBA",
+  components: { Jumbotron },
+  data: () => ({
+    jumbrotronImage,
+    heading,
+    content,
+  }),
 };
 </script>
